612cf04588a30dc18f165fd2cf8722d09d8f47f8
milan <bhattmilan7498@gmail.com>
2019-03-06 16:33:32 +0530

    for pull

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -22,6 +22,7 @@ export class ProjectDetailComponent implements OnInit {
 		editorData: 'Enter comments here'
 	};
 	url;
+	currentUser = JSON.parse(localStorage.getItem('currentUser'));
 	task;
 	project;
 	comment;
