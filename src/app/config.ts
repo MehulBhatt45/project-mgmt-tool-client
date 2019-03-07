0f046ad3e7269652234cd46b1c0e688d66319bb8
SwatiChauhan20 <chauhan.swati104@gmail.com>
2019-03-07 12:13:58 +0530

    header design

--- src/app/config.ts
+++ src/app/config.ts
@@ -5,14 +5,14 @@ const baseUrl = "http://206.189.231.135:4000/";
 
 // const baseUrl = "http://132.140.160.60:4000/";
 
-const baseUrl = "http://132.140.160.60:4000/";
+// const baseUrl = "http://132.140.160.60:4000/";
 
 // const baseUrl = "http://localhost:4000/";
 
 
 export const config = {
-	baseApiUrl: baseUrl,
-	"priorityList": [
+    baseApiUrl: baseUrl,
+    "priorityList": [
         { id: "1", value: 'low', colorCode: 'blue' },
         { id: "2", value: 'medium', colorCode: 'yellow' },
         { id: "3", value: 'high', colorCode: 'red' }
