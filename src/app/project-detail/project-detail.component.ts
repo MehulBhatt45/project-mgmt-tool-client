2ee494a17eb8aeff691c9066d2da2df6ef57821a
Mehul Bhatt <mehul.2287884@gmail.com>
2019-03-08 14:55:14 +0530

    makingh comments work

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -11,7 +11,7 @@ import { ChildComponent } from '../child/child.component';
 
 declare var $ : any;
 import * as _ from 'lodash';
-
+import { CommentService } from '../services/comment.service';
 
 
 @Component({
@@ -29,7 +29,7 @@ export class ProjectDetailComponent implements OnInit {
 	};
 	url;
 	searchText;
-
+	newTask;
 	task;
 	projects: any;
 	project;
@@ -45,7 +45,8 @@ export class ProjectDetailComponent implements OnInit {
 	files:FileList;
 	
 	constructor(public _projectService: ProjectService, private route: ActivatedRoute,
-		public _alertService: AlertService, public searchTextFilter: SearchTaskPipe) {
+		public _alertService: AlertService, public searchTextFilter: SearchTaskPipe,
+		public _commentService: CommentService) {
 
 		this.route.params.subscribe(param=>{
 			this.projectId = param.id;
@@ -298,18 +299,18 @@ export class ProjectDetailComponent implements OnInit {
 	}
 
 	editTask(task){
-		this.task = task;
+		this.newTask = task;
 		this.modalTitle = 'Edit Item';
 		$('.datepicker').pickadate();
 		$('#input_starttime').pickatime({});
-		$('#editModel').modal('show');
+		$('#exampleModalPreviewLabel').modal('show');
 	}
 
 	updateTask(task){
 		task.assignTo = this.editTaskForm.value.assignTo;
 		console.log("update =====>",task);
 		this._projectService.updateTask(task).subscribe((res:any)=>{
-			$('#editModel').modal('hide');
+			$('#exampleModalPreviewLabel').modal('hide');
 		},err=>{
 			console.log(err);
 			
@@ -317,16 +318,16 @@ export class ProjectDetailComponent implements OnInit {
 		
 	}
 
+	getEmptyTask(){
+		return { title:'', desc:'', assignTo: '', status: 'to do', priority: 'low' };
+	}
+
 	addItem(option){
-		this.loader=true;
-		setTimeout(()=>{
-			this.task = { title:'', desc:'', assignTo: '', status: 'to do', priority: 'low' };
-			this.modalTitle = 'Add '+option;
-			$('.datepicker').pickadate();
-			$('#input_starttime').pickatime({});
-			$('#editModel').modal('show');
-			this.loader=false;
-		},1000);
+		this.newTask = { title:'', desc:'', assignTo: '', status: 'to do', priority: 'low' };
+		this.modalTitle = 'Add '+option;
+		$('.datepicker').pickadate();
+		$('#input_starttime').pickatime({});
+		$('#exampleModalPreviewLabel').modal('show');
 	}
 
 
@@ -341,7 +342,7 @@ export class ProjectDetailComponent implements OnInit {
 		// subUrl = _.includes(task.uniqueId, 'TSK')?"task/add-task/":'' || _.includes(task.uniqueId, 'BUG')?"bug/add-bug/":'' || _.includes(task.uniqueId, 'ISSUE')?"issue/add-issue/":'';
 		// console.log(subUrl);
 		this._projectService.addTask(task).subscribe((res:any)=>{
-			$('#editModel').modal('hide');
+			$('#exampleModalPreviewLabel').modal('hide');
 			// this.getProject(this.projectId);
 		},err=>{
 			console.log(err);
@@ -366,8 +367,16 @@ export class ProjectDetailComponent implements OnInit {
 
 
 
-	sendComment(){
+	sendComment(taskId){
 		console.log(this.comment);
+		var data = {content:this.comment, userId: this.currentUser._id, taskId: taskId};
+		this._commentService.addComment(data).subscribe(res=>{
+			console.log(res);
+			this.comment = "";
+			this.model.editorData = 'Enter comments here';
+		},err=>{
+			console.error(err);
+		})
 	}
 	searchTask(){
 		console.log("btn tapped");
