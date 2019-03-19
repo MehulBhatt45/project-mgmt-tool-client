122f980e5b588ea2897ac8137188035e86c099ce
Mehul Bhatt <mehul.2287884@gmail.com>
2019-03-19 09:50:19 +0530

    edit project with team

--- src/app/config.ts
+++ src/app/config.ts
@@ -7,7 +7,7 @@ const baseUrl = "http://132.140.160.60:4000/";
 
 // const baseUrl = "http://localhost:4000/";
 
-const baseMediaUrl = "http://132.140.160.60/project_mgmt_tool/server/uploads/";
+// const baseMediaUrl = "http://132.140.160.60/project_mgmt_tool/server/uploads/";
 
 
 
@@ -22,7 +22,7 @@ const baseMediaUrl = "http://132.140.160.60/project_mgmt_tool/server/uploads/";
 
 // 
 
-// const baseMediaUrl = "http://localhost/project_mgmt_tool/server/";
+const baseMediaUrl = "http://localhost/project_mgmt_tool/server/";
 
 
 // const baseMediaUrl = "http://206.189.231.135/project-mgmt-tool-server/uploads/";
