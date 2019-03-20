018e38af49f4cf8d6789ff55261595fed73aed4d
Akshita_Karetiya <karetiyaakshita@gmail.com>
2019-03-20 09:26:06 +0530

    commit for pull

--- src/app/all-leave-app/all-leave-app.component.ts
+++ src/app/all-leave-app/all-leave-app.component.ts
@@ -10,11 +10,12 @@ declare var $ : any;
   styleUrls: ['./all-leave-app.component.css']
 })
 export class AllLeaveAppComponent implements OnInit {
-
+  tracks:any;
   leaves;
   leaveApp;
   acceptedLeave;
   rejectedLeave;
+  id;
   currentUser = JSON.parse(localStorage.getItem('currentUser'));
 
   // apps;
@@ -24,7 +25,10 @@ export class AllLeaveAppComponent implements OnInit {
   ngOnInit() {
   	this.getLeaves();
     this.leavesByUserId();
+
+    // this.$filter('orderBy')(this.leaveApp,'startingDate');
   }
+
   getLeaves(){
   	this._projectservice.pendingLeaves().subscribe(res=>{
   		console.log("data avo joye==========>",res);
@@ -39,7 +43,6 @@ export class AllLeaveAppComponent implements OnInit {
       console.log(err);
     })
   }
-
   leaveAccepted(req){
     var body;
     console.log("dsfdsbgfdf",this.leaveApp);
@@ -100,4 +103,50 @@ export class AllLeaveAppComponent implements OnInit {
     })
   }
 
+  statusOfLeave(){
+    this._projectservice.pendingLeaves().subscribe(res=>{
+      console.log("data is ============>",res);
+      this.leaveApp = res;
+      _.forEach(this.leaveApp , (leave)=>{
+        leave.startingDate = moment(leave.startingDate).format('YYYY-MM-DD');
+        leave.endingDate = moment(leave.endingDate).format('YYYY-MM-DD');
+      })
+      //this.dueDate = moment().add({days:task.dueDate,months:0}).format('YYYY-MM-DD HH-MM-SS'); 
+      console.log("applicationsss==>",this.leaveApp);
+    },err=>{
+      console.log(err);
+    })
+
+  }
+  statusOfLeaveForAdmin(leave){
+    var obj ={ email : JSON.parse(localStorage.getItem('currentUser')).email};
+    console.log("email of login user",obj);
+    console.log("current User is");
+    this._projectservice.getAllLeaves().subscribe(res=>{
+      console.log("all data==========>",res);
+      this.leaveApp = res;
+      _.forEach(this.leaveApp , (leave)=>{
+        leave.startingDate = moment(leave.startingDate).format('YYYY-MM-DD');
+        // let _sortedDates = leave.startingDate.sort(function(a, b){
+        //   return moment(b).format('YYYY-MM-DD')- moment(a).format('YYYY-MM-DD')
+        // });
+        // leave.startingDate.sort(function(a, b) {
+        //     a = new leave.startingDate(a.dateModified).format('YYYY-MM-DD');
+        //     b = new leave.startingDate(b.dateModified).format('YYYY-MM-DD');
+        //     return a>b ? -1 : a<b ? 1 : 0;
+        //   });
+          // console.log("change date=============>",leave.startingDate);
+          leave.endingDate = moment(leave.endingDate).format('YYYY-MM-DD');
+        })
+      console.log("statussssssss",this.leaves);
+      console.log("applicationsss==>",this.leaveApp);
+    },err=>{
+      console.log(err);
+    })
+
+
+  }
+
 }
+
+
