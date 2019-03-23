b0222293dcff003e5057aa72e686c8bc05bdf6eb
vivek bharda <vivekkbharda@gmail.com>
2019-03-23 10:41:04 +0530

    conflict-solved

--- src/app/user-summary/user-summary.component.ts
+++ src/app/user-summary/user-summary.component.ts
@@ -264,6 +264,46 @@ export class UserSummaryComponent implements OnInit {
 
 					this.loader = false;
 					setTimeout(()=>{
+
+
+						var ctx = document.getElementById("myChart");
+						var myChart = new Chart(ctx, {
+							type: 'bar',
+							data: {
+								labels: ["to do", "In Progress", "testing", "Complete"],
+								datasets: [{
+									label: '# of Tasks',
+									// data:[7,14,43,33],
+									data:this.getTaskCountEachTrack(this.tracks.id),
+									backgroundColor: [
+									'rgba(255, 99, 132, 0.2)',
+									'rgba(54, 162, 235, 0.2)',
+									'rgba(255, 206, 86, 0.2)',
+									'rgba(75, 192, 192, 0.2)'
+
+									],
+									borderColor: [
+									'rgba(255,99,132,1)',
+									'rgba(54, 162, 235, 1)',
+									'rgba(255, 206, 86, 1)',
+									'rgba(75, 192, 192, 1)'
+
+									],
+									borderWidth: 1
+								}]
+							},
+							options: {
+
+								scales: {
+									yAxes: [{
+										ticks: {
+											beginAtZero: true
+										}
+									}]
+								}
+							}
+						});
+
 						
 
 						var ctxP = document.getElementById("pieChart1");
@@ -363,12 +403,28 @@ export class UserSummaryComponent implements OnInit {
 					this.loader = false;
 				})
 
-			},1000);
+},1000);
 function custom_sort(a, b) {
 	return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
 }
 }
 
+getTaskCountEachTrack(tracks){
+
+	var userId = this.userId;
+	console.log("uid+_+_+{}{}====",userId);
+
+	// return _.filter(this.project, function(o) { if (o.assignTo._id == userId && o.status == status) return o }).length;
+	var count1 = [];
+	_.forEach(tracks,track=>{
+
+		count1.push(_.filter(this.project, function(o) { if (o.assignTo._id == userId && o.status == track.id) return o }).length);
+	});
+	console.log("count1---------==========",count1);
+	return count1;
+
+
+}
 getTaskCount(){
 	// console.log("userId===-=-={}{}{}{}{}",userId);
 	var userId = this.userId;
