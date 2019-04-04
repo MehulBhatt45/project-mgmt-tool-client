330f7b355373cef79496cf078a07728e27563406
rajgohel0001 <rajgohel855@gmail.com>
2019-04-04 10:28:18 +0530

    commit

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { MessagingService } from '../services/messaging.service';
+import { Router,ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-notification',
-  templateUrl: './notification.component.html',
-  styleUrls: ['./notification.component.css']
+	selector: 'app-notification',
+	templateUrl: './notification.component.html',
+	styleUrls: ['./notification.component.css']
 })
 export class NotificationComponent implements OnInit {
 
-  constructor() { }
+	constructor(public _messagingservice:MessagingService,public route:ActivatedRoute,public router:Router) {
 
-  ngOnInit() {
-  }
+	}
 
+	ngOnInit() {
+		this.get();
+	}
+	get(){
+		var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+		console.log("res-=-=",currentUser);
+	}
 }
