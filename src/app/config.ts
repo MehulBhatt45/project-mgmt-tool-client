e00a60d01a786e79850e9b7cf472a80eecdc42c9
vivekbharda <vivekkbharda@gmail.com>
2019-04-09 10:32:37 +0530

    for pull

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,10 +1,10 @@
 
 
 const baseUrl = "https://raoinfotech-conduct.tk:4001/";
-// const baseUrl = "http://132.140.160.113:4000/";
+// const baseUrl = "http://132.140.160.114:4001/";
 // const baseUrl = "http://localhost:4000/";
 
-// const baseMediaUrl = "http://132.140.160.113/project_mgmt_tool/server/uploads/";
+// const baseMediaUrl = "http://132.140.160.114/project_mgmt_tool/server/uploads/";
 
 
 const baseMediaUrl = "https://raoinfotech-conduct.tk/server/uploads/";
