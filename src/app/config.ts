50c173f7f757a28750e3c09cf7d72164e3c15e65
vivekbharda <vivekkbharda@gmail.com>
2019-04-12 18:04:12 +0530

    conflict resolved

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const baseUrl = "https://raoinfotech-conduct.tk:4001/";
 // const baseUrl = "http://132.140.160.79:4001/";
 // const baseUrl = "http://localhost:4001/";
@@ -6,21 +6,8 @@ const baseUrl = "https://raoinfotech-conduct.tk:4001/";
 // const baseMediaUrl = "http://132.140.160.79/project_mgmt_tool/server/uploads/";
 const baseMediaUrl = "https://raoinfotech-conduct.tk/server/uploads/";
 // const baseMediaUrl = "http://localhost/project_mgmt_tool/server/uploads/";
-=======
-
-// const baseUrl = "https://raoinfotech-conduct.tk:4001/";
-// const baseUrl = "http://132.140.160.61:4001/";
-const baseUrl = "http://localhost:4001/";
-// 
-
-// const baseMediaUrl = "http://132.140.160.61/project_mgmt_tool/server/uploads/";
 
 
-// const baseMediaUrl = "https://raoinfotech-conduct.tk/server/uploads/";
-
-
-const baseMediaUrl = "http://localhost/project_mgmt_tool/server/uploads/";
->>>>>>> 9994ffbd844af40b7448b51ebea8e717f0f20d43
 
 
 
