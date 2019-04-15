5ca60d0bc3297918d1274266830e6e58199a7664
SwatiChauhan20 <[chauhan.swati104@gmail.com]>
2019-04-15 11:21:20 +0530

    user profile design

--- src/app/config.ts
+++ src/app/config.ts
@@ -5,8 +5,8 @@ const baseUrl = "https://raoinfotech-conduct.tk:4001/";
 // const baseUrl = "http://localhost:4001/";
 
 // const baseMediaUrl = "http://132.140.160.79/project_mgmt_tool/server/uploads/";
-// const baseMediaUrl = "https://raoinfotech-conduct.tk/server/uploads/";
-const baseMediaUrl = "http://localhost/project_mgmt_tool/server/uploads/";
+const baseMediaUrl = "https://raoinfotech-conduct.tk/server/uploads/";
+// const baseMediaUrl = "http://localhost/project_mgmt_tool/server/uploads/";
 
 
 
