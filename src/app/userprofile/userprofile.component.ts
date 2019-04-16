5af1229dce67bb8192642910954eb6978c2e0349
vishalpankhaniya <vishal.pankhaniya786@gmail.com>
2019-04-16 18:01:04 +0530

    userprofile dynamic

--- src/app/userprofile/userprofile.component.ts
+++ src/app/userprofile/userprofile.component.ts
@@ -38,6 +38,7 @@ export class UserprofileComponent implements OnInit {
 	task;
 	currentUser = JSON.parse(localStorage.getItem('currentUser'));
 	baseMediaUrl = config.baseMediaUrl;
+	pmanagerId = JSON.parse(localStorage.getItem('currentUser'));
 	
 	constructor(private route: ActivatedRoute,public _alertService: AlertService,
 		private router: Router, public _projectservice: ProjectService, public _loginService: LoginService) { 
@@ -58,133 +59,128 @@ export class UserprofileComponent implements OnInit {
 		this.route.params.subscribe(param=>{
 			this.userId = param.id;
 			this.projectId = param.id;
-			// this.getDeveloperById(this.userId);
-			// this.getAllProjects(this.projectId);
 		});
 		this.createEditEmail();
-		// this.projectSelected(this.item);
-		// this.getAllDevelopers();
-		this.getAllProjects();
+		this.getProjects();
 
 	}
-	getAllProjects(){
-		this._projectservice.getProjects().subscribe(res=>{
-			console.log("all projects =====>" , res);
-			var userId = JSON.parse(localStorage.getItem('currentUser'))._id;
-			console.log("current user ====>" , userId);
-			// this.pro = res.userId;
-			// console.log("project detail===>>>>",this.pro);
-			this.projects = res;
-			console.log(this.projects);
-			console.log(this.projects[0].pmanagerId._id);
-			// if (projects[0].) {
 
-				// }
-				// console.log("team===>",this.projects[0].Teams);
-				_.forEach(this.projects , (task)=>{
-					_.forEach(task.Teams , (project)=>{
-						if(project._id == userId){
-
-							this.projectArr.push(task);
+	getProjects(){
+		this._projectservice.getProjects().subscribe((res:any)=>{
+			if(this.currentUser.userRole == 'projectManager'){
+				this.projects = [];
+				console.log("this.projects",this.projects);
+				_.forEach(res,(project)=>{
+					_.forEach(project.pmanagerId,(pid)=>{
+						if(pid._id == this.currentUser._id){
+							this.projects.push(project);
 						}
 					})
 				})
-				for(var i=0;i<this.projects.length;i++){
-					this.finalArr.push(this.projectArr[i]);
-					console.log("response======>",this.finalArr);
-				}	
-			},err=>{
-				this._alertService.error(err);
-				console.log(err);
-			})
-	}
-	// getDeveloperById(id){
-		// 	this._loginService.getUserById(id).subscribe((res:any)=>{
-			// 		this.currentUser = res;
-			// 		console.log("current user =============>",res);
-			// 		var userId = JSON.parse(localStorage.getItem('currentUser'))._id;
-			// 		console.log(" currentUser  ====>" , userId);
-			// 	},(err:any)=>{
-				// 	})
-				// }
-
-				getAllDevelopers(){
-					this._projectservice.getAllDevelopers().subscribe(res=>{
-						this.developers = res;
-						console.log("Developers",this.developers);
-					},err=>{
-						console.log("Couldn't get all developers ",err);
-						this._alertService.error(err);
-					})
-				}
-				openModel(task){
-					$('#editEmailModel').modal('show');
-					this.getProjectByPmanagerId();
-				}
-				projectSelected(item){
-					// console.log("item",item);
-					// this.ProjectIdTest = item._id;
-					if(item && item._id){
-						_.forEach(item.Teams,(all)=>{
-							console.log("all",all._id);
-							this.total.push(all._id);
-						})
-						this.teams =item.Teams;
-						console.log(this.teams);
-
-						$(".progress").addClass("abc");
-						setTimeout(()=>{
-							// this.loader = false;
-							$(".progress").removeClass("abc");
-							this.task.projectId = item._id;	
-							this.developers = this.projects[_.findIndex(this.projects, {_id: item._id})].Teams;
-							// console.log(this.developers);
-						},3000);
-					}else{
-						this.editTEmail.reset();
-					}
-				}
-				getProjectByPmanagerId(){
-					this._projectservice.getProjectByPmanagerId(this.currentUser._id).subscribe((res:any)=>{
-						this.currentUser = res;
-						console.log("current====>",this.currentUser);
-
-					})
-				}
-
-				addNotification(editTEmail){
-					// console.log("CurrentUserId========>",this.currentUser._id);
-					editTEmail.value['pmanagerName'] = JSON.parse(localStorage.getItem('currentUser')).name;
-					console.log("editTEmail" , editTEmail.value);
-					
-					this._projectservice.addNotification(editTEmail.value).subscribe((res:any)=>{
-						console.log(res);
-
-						// this.notification = this.myObject;
-						// console.log(this.myObject);
-					})
-				}
-				uploadFile(e){
-					console.log("file============>",e.target.files);
-					var userId = JSON.parse(localStorage.getItem('currentUser'))._id;
-					console.log("userId===============>",userId);
-					this.files = e.target.files;
-					console.log("files===============>",this.files);
-					this._loginService.changeProfilePicture(this.files, userId).subscribe((res:any)=>{
-						console.log("resss=======>",res);
-						Swal.fire({type: 'success',title: 'profile Picture Updated Successfully',showConfirmButton:false,timer: 2000})
-
-						setTimeout(()=>{
-							this.currentUser = res;
-							localStorage.setItem('currentUser', JSON.stringify(this.currentUser));
-						},1000);
-					},error=>{
-						console.log("errrorrrrrr====>",error);
-						Swal.fire('Oops...', 'Something went wrong!', 'error')
-					});  
-				}
-
+				console.log("IN If=========================================",this.projects);
 			}
+			else{
+				this.projects = [];
+				_.forEach(res, (p)=>{
+					_.forEach(p.Teams, (user)=>{
+						if(user._id == this.currentUser._id)
+							this.projects.push(p);
+					})
+				});
+				console.log("IN Else=========================================",this.projects);
+			}
+		},err=>{
+			Swal.fire('Oops...', 'Something went wrong!', 'error')  
+		});
+	}
+	
+	getDeveloperById(id){
+		this._loginService.getUserById(id).subscribe((res:any)=>{
+			this.currentUser = res;
+			console.log("current user =============>",res);
+			var userId = JSON.parse(localStorage.getItem('currentUser'))._id;
+			console.log(" currentUser  ====>" , userId);
+		},(err:any)=>{
+		})
+	}
+
+	getAllDevelopers(){
+		this._projectservice.getAllDevelopers().subscribe(res=>{
+			this.developers = res;
+			console.log("Developers",this.developers);
+		},err=>{
+			console.log("Couldn't get all developers ",err);
+			this._alertService.error(err);
+		})
+	}
+	openModel(task){
+		$('#editEmailModel').modal('show');
+		this.getProjectByPmanagerId();
+	}
+	projectSelected(item){
+		// console.log("item",item);
+		// this.ProjectIdTest = item._id;
+		if(item && item._id){
+			_.forEach(item.Teams,(all)=>{
+				console.log("all",all._id);
+				this.total.push(all._id);
+			})
+			this.teams =item.Teams;
+			console.log(this.teams);
+
+			$(".progress").addClass("abc");
+			setTimeout(()=>{
+				// this.loader = false;
+				$(".progress").removeClass("abc");
+				this.task.projectId = item._id;	
+				this.developers = this.projects[_.findIndex(this.projects, {_id: item._id})].Teams;
+				// console.log(this.developers);
+			},3000);
+		}else{
+			this.editTEmail.reset();
+		}
+	}
+	getProjectByPmanagerId(){
+		this._projectservice.getProjectByPmanagerId(this.currentUser._id).subscribe((res:any)=>{
+			this.currentUser = res;
+			console.log("current====>",this.currentUser);
+
+		})
+	}
+
+	addNotification(editTEmail){
+		// console.log("CurrentUserId========>",this.currentUser._id);
+		editTEmail.value['pmanagerName'] = JSON.parse(localStorage.getItem('currentUser')).name;
+		console.log("editTEmail" , editTEmail.value);
+
+		this._projectservice.addNotification(editTEmail.value).subscribe((res:any)=>{
+			console.log(res);
+
+			// this.notification = this.myObject;
+			// console.log(this.myObject);
+		})
+	}
+	uploadFile(e){
+		console.log("file============>",e.target.files);
+		var userId = JSON.parse(localStorage.getItem('currentUser'))._id;
+		console.log("userId===============>",userId);
+		this.files = e.target.files;
+		console.log("files===============>",this.files);
+		this._loginService.changeProfilePicture(this.files, userId).subscribe((res:any)=>{
+			console.log("resss=======>",res);
+			Swal.fire({type: 'success',title: 'profile Picture Updated Successfully',showConfirmButton:false,timer: 2000})
+
+			setTimeout(()=>{
+				this.currentUser = res;
+				localStorage.setItem('currentUser', JSON.stringify(this.currentUser));
+			},1000);
+		},error=>{
+			console.log("errrorrrrrr====>",error);
+			Swal.fire('Oops...', 'Something went wrong!', 'error')
+		});  
+	}
+
+}
 
 
 
