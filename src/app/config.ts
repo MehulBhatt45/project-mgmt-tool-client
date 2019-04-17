c318e6860c9394fee0b929e1eca254df96a843c1
Akshita_Karetiya <karetiyaakshita@gmail.com>
2019-04-17 10:11:28 +0530

    search bar in all leave app and veiw attachment modal in my leave

--- src/app/config.ts
+++ src/app/config.ts
@@ -8,13 +8,13 @@ const baseUrl = "http://localhost:4001/";
 // const baseMediaUrl = "http://132.140.160.79/project_mgmt_tool/server/uploads/";
 // const baseMediaUrl = "https://raoinfotech-conduct.tk/server/uploads/";
 const baseMediaUrl = "http://localhost/project_mgmt_tool/server/uploads/";
-
+// 
 
 export const config = {
     baseApiUrl: baseUrl,
     baseMediaUrl: baseMediaUrl,
     "priorityList": [
-    { id: "1", value: 'low', colorCode: 'blue' },
+    // { id: "1", value: 'low', colorCode: 'blue' },
     { id: "2", value: 'medium', colorCode: 'yellow' },
     { id: "3", value: 'high', colorCode: 'red' }
     ],
