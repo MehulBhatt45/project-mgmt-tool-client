03f36f1481450a55aa4f4f88982754864bba575d
Akshita_Karetiya <karetiyaakshita@gmail.com>
2019-05-03 19:16:54 +0530

    console error solve

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,13 +1,13 @@
 
 // const baseUrl = "https://raoinfotech-conduct.tk:4001/";
-// const baseUrl = "http://132.140.160.114:4001/";
-const baseUrl = "http://localhost:4001/";
+const baseUrl = "http://132.140.160.114:4001/";
+// const baseUrl = "http://localhost:4001/";
 // const baseUrl = "http://192.168.43.66:4001/";
 
 
-// const baseMediaUrl = "http://132.140.160.114/project_mgmt_tool/server/uploads/";
+const baseMediaUrl = "http://132.140.160.114/project_mgmt_tool/server/uploads/";
 // const baseMediaUrl = "https://raoinfotech-conduct.tk/server/uploads/";
-const baseMediaUrl = "http://localhost/project_mgmt_tool/server/uploads/";
+// const baseMediaUrl = "http://localhost/project_mgmt_tool/server/uploads/";
 // const baseMediaUrl = "http://192.168.43.66/project_mgmt_tool/server/uploads/";
 
 
