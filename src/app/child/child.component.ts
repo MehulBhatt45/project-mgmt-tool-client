a2c93cd59bfa560dcd012519876dda915be5e723
vivekbharda <vivekkbharda@gmail.com>
2019-05-04 18:24:28 +0530

    for pull

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -270,7 +270,7 @@ export class ChildComponent  implements OnInit{
     })
   }
 
- 
+  
   getTitle(name){
     if(name){
       var str = name.split(' ');
@@ -457,7 +457,71 @@ export class ChildComponent  implements OnInit{
     }
   }
 
-  get f() { return this.editTaskForm.controls; }
+  get f() { return this.editTaskForm.value; }
+
+
+  saveTheData(task){
+    
+    this.submitted = true;
+    if (this.editTaskForm.invalid) {
+      return;
+    }
+    this.loader = true;
+    task['projectId']= this.projectId;
+    console.log("projectId=========>",this.projectId);
+    task.priority = Number(task.priority); 
+    task['type']= _.includes(this.modalTitle, 'Task')?'TASK':_.includes(this.modalTitle, 'Bug')?'BUG':_.includes(this.modalTitle, 'Issue')?'ISSUE':''; 
+    console.log("estimated time=====>",task.estimatedTime);
+    // task.images = $("#images").val();
+    console.log("images====>",task.images);
+    console.log(task.dueDate);
+    task.dueDate = moment().add(task.dueDate, 'days').format('YYYY-MM-DD');
+    task['createdBy'] = JSON.parse(localStorage.getItem('currentUser'))._id;
+    console.log("task ALL details",task);
+    let data = new FormData();
+    _.forOwn(task, function(value, key) {
+      data.append(key, value)
+    });
+    if(this.files.length>0){
+      for(var i=0;i<this.files.length;i++){
+        data.append('fileUpload', this.files[i]);  
+      }
+    }
+    this._projectService.addTask(data).subscribe((res:any)=>{
+      console.log("response task***++",res);
+      let name = res.assignTo.name;
+      console.log("assign to name>>>>>>>>>>>><<<<<<<<",name);
+      Swal.fire({type: 'success',
+        title: 'Task Added Successfully to',
+        text: name,
+        showConfirmButton:false,
+        timer: 2000,
+        // position: 'top-end'
+      })
+      this.getProject(res.projectId._id);
+      $('#save_changes').attr("disabled", false);
+      $('#refresh_icon').css('display','none');
+      $('#itemManipulationModel').modal('hide');
+      this.newTask = this.getEmptyTask();
+      this.editTaskForm.reset();
+      this.files = this.url = [];
+      // this.assignTo.reset();
+      this.loader = false;
+    },err=>{
+      Swal.fire({
+        type: 'error',
+        title: 'Ooops',
+        text: 'Something went wrong',
+        animation: false,
+        customClass: {
+          popup: 'animated tada'
+        }
+      })
+      //$('#alert').css('display','block');
+      console.log("error========>",err);
+    });
+  }
+
 
   updateTask(task){
     this.submitted = true;
