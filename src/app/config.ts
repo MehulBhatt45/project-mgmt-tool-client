70df76b522bbaaedc46151eb07778831f3f1cd92
SwatiChauhan20 <[chauhan.swati104@gmail.com]>
2019-05-04 17:56:42 +0530

    changes designing

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,14 +1,5 @@
 // const baseUrl = "https://raoinfotech-conduct.tk:4001/";
-
 const baseUrl = "http://132.140.160.114:4001/";
-// const baseUrl = "http://localhost:4001/";
-// const baseUrl = "http://192.168.43.66:4001/";
-// 
-// 
-// const baseMediaUrl = "http://132.140.160.114/project_mgmt_tool/server/uploads/";
-
-
-// const baseUrl = "http://132.140.160.114:4001/";
 // const baseUrl = "http://localhost:4001/";
 
 // const baseMediaUrl = "https://raoinfotech-conduct.tk/server/uploads/";
