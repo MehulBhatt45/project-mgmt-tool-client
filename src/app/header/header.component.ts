a28eff8e02701181573b624cf2541510c30ee058
SwatiChauhan20 <[chauhan.swati104@gmail.com]>
2019-05-06 12:46:31 +0530

    overlay remove

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -82,9 +82,7 @@ export class HeaderComponent implements OnInit {
 		// document.getElementById("slide-out").style.width = "250px";
 	}
 	closeNav() {
-		// document.getElementById("slide-out").style.width = "0";
 		console.log("Have to close menu");
-		 // $('#sidenav-overlay').remove();
 		 $("#sidenav-overlay").css("display", "none");
       $('#slide-out').attr('style','transform: translateX(-100%); transition: all 200ms cubic-bezier(0.25, 0.46, 0.45, 0.94)');
 	}
